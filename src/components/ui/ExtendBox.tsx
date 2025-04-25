275c18c16db50ac01e186cc1840468e694d3501e
Muhammd Dawood Iqbal <dawoodjand356@gmail.com>
2025-04-25 17:27:17 +0500

    progress commit

--- src/components/ui/ExtendBox.tsx
+++ src/components/ui/ExtendBox.tsx
@@ -1,13 +1,10 @@
 import React, { useState, useRef } from 'react'
 import { FaMinus, FaPlus } from 'react-icons/fa'
-import gsap from 'gsap'
-import { useGSAP } from '@gsap/react'
 
 
 const ExtendBox = () => {
 
     const [open, setOpen] = useState(false)
-    const spanRef = useRef(null)
   return (
     <div className='w-full h-[28vh] bg-slate-400 drop-shadow-[0_6px_rgba(0,0,0,0.6)] rounded-[45px]'>
         <div className='flex items-center justify-between w-full px-5'>
