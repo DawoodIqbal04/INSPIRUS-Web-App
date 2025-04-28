54fee009f02235c6e9275e384a5b99194ba135f1
Muhammd Dawood Iqbal <dawoodjand356@gmail.com>
2025-04-28 18:07:41 +0500

    marker animation commit

--- src/components/ui/Marker.tsx
+++ src/components/ui/Marker.tsx
@@ -1,11 +1,36 @@
 import { grotesk, sans } from "@/font";
-import React from "react";
+import React, {useRef} from "react";
+import gsap from 'gsap'
+import { useGSAP } from "@gsap/react";
+import ScrollTrigger from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger)
+
 
 const Marker = ({title, desc} : {title: string, desc: string}) => {
+
+  const markerRef = useRef(null);
+
+
+  useGSAP(() => {
+    gsap.to(markerRef.current, {
+      x: 300,
+      duration: 1,
+      ease: "power2.inOut",
+      display: 'none',
+      scrollTrigger: {
+        trigger: markerRef.current,
+        start: "top 80%",
+
+      }
+    })
+  });
+
   return (
     <div className="flex items-center justify-start gap-10">
-      <h1 className={`text-4xl ${grotesk.className}`}>
-        <mark className="bg-primary px-2 rounded-lg">{title}</mark>
+      <h1 className={`text-4xl ${grotesk.className} relative overflow-hidden flex items-center justify-center`}>
+        <mark className="bg-primary px-2 rounded-lg ">{title}</mark>
+        <div ref={markerRef} className="absolute w-full h-full bg-whital top-0"></div>
       </h1>
       <p className={`${sans.className} tracking-wide w-[45%]`}>
         {desc}
